b8e31970443aaa2f13fd764593a6d1fb1ebfe488
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-04 15:27:59 -0500

    Add Payment attributes and constructor

--- yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/Payment.java
+++ yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/Payment.java
@@ -1,4 +1,16 @@
 package com.project.yapayspringboot.model;
 
+import java.util.UUID;
+
 public class Payment {
+    private UUID id = UUID.randomUUID();
+    private Company company;
+    private QR qr;
+    private float totalAmount;
+
+    public Payment(String companyName, String companyPhone, float amount) {
+        company = new Company(companyName, companyPhone);
+        totalAmount = amount;
+        qr = new QR(id, company, totalAmount);
+    }
 }
