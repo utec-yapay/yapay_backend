6e292b6fa115ed7b52ee59a02d8f27d5e3a6640c
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-04 15:18:02 -0500

    Change company id to phone number

--- yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/Company.java
+++ yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/Company.java
@@ -3,16 +3,14 @@ package com.project.yapayspringboot.model;
 import java.util.UUID;
 
 public class Company {
-    private UUID id = UUID.randomUUID();
-    private String name;
     private String phone;
+    private String name;
 
     public Company(String companyName, String phoneNumber) {
         this.name = companyName;
         this.phone = phoneNumber;
     }
 
-    public UUID getId() { return id; }
     public String getName() { return name; }
     public String getPhone() { return phone;  }
 
