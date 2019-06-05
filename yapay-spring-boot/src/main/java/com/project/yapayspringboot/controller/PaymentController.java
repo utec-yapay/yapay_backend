9647d561c6dc14713ed774defd7224335cd0dd08
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-05 14:50:30 -0500

    Add payment creation with POST in Payment Controller

--- yapay-spring-boot/src/main/java/com/project/yapayspringboot/controller/PaymentController.java
+++ yapay-spring-boot/src/main/java/com/project/yapayspringboot/controller/PaymentController.java
@@ -1,4 +1,20 @@
 package com.project.yapayspringboot.controller;
 
+import com.project.yapayspringboot.model.Payment;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PaymentController {
+
+    @PostMapping("/payments")
+    public Payment createPayment(@RequestBody Payment payment){
+        return payment;
+    }
+
+
+
+
 }
