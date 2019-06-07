23f36a93c54904afda26cca98b928de71f34e02c
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-07 16:05:33 -0500

    Set database columns to NOT NULL and confirmed default value to false

--- yapay-database/YaPayDB-script.sql
+++ yapay-database/YaPayDB-script.sql
@@ -5,13 +5,13 @@ CREATE database YaPayDB;
 \c YaPayDB
 */
 
-CREATE TABLE payments (
-	payment_id uuid,
-	company_id uuid,
-	company_name varchar(225),
-	total float,
-	payment_date timestamp,
-	confirmed bool,
-	PRIMARY KEY (payment_id)
-);
-
+CREATE TABLE public.payments
+(
+    payment_id uuid NOT NULL,
+    company_id uuid NOT NULL,
+    company_name character varying(225)  NOT NULL,
+    total double precision NOT NULL,
+    payment_date timestamp without time zone NOT NULL,
+    confirmed boolean NOT NULL DEFAULT false,
+    CONSTRAINT payments_pkey PRIMARY KEY (payment_id)
+)
