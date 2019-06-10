d7ae4ffc1483128f197aa601b244eef13134c379
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-09 22:56:25 -0500

    Implement payment controller methods to work with payment dao

--- yapay-spring-boot/src/main/java/com/project/yapayspringboot/controller/PaymentController.java
+++ yapay-spring-boot/src/main/java/com/project/yapayspringboot/controller/PaymentController.java
@@ -1,36 +1,58 @@
 package com.project.yapayspringboot.controller;
 
+import com.project.yapayspringboot.dao.PaymentDao;
 import com.project.yapayspringboot.model.Payment;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Hashtable;
-import java.util.UUID;
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PaymentController {
     // TODO: Error handling
 
-    @PostMapping("/payments")
-    public String createPayment(@RequestBody Payment newpayment){
-        // TODO: Valid request body
+    @Resource
+    PaymentDao paymentDao;
 
+    @GetMapping("/payments")
+    public List<Payment> getAllPayments(){
+        return paymentDao.selectAllPayments();
+    }
+
+    @PostMapping("/payments")
+    public String createPayment(@RequestBody Map<String, Object> payload) throws IllegalArgumentException{
         /* Creates payment and returns the JSON
-        *  Web Token (JWT) that will be use to
-        *  create a QR.
-        *
-        *  JWT payload includes: paymentId, companyPhone,
-        *                        companyName, amount
-        *
-        *  Throws exception if request body is missing any
-        *  of the parameters: companyName, companyPhone, amount
-        */
+         *  Web Token (JWT) that will be use to
+         *  create a QR.
+         *
+         *  JWT payload includes: paymentId, companyPhone,
+         *                        companyName, amount
+         *
+         *  Throws exception if request body is missing any
+         *  of the parameters: companyName, companyPhone, amount
+         */
+
+        Payment newpayment;
+        try{
+            newpayment = new Payment((String) payload.get("companyName"),
+                    (String) payload.get("companyPhone"),
+                    new Float( (Double) payload.get("amount")));
+        }catch (Exception e){
+            throw new IllegalArgumentException("Invalid payment");
+        }
+
+
+        Long id = paymentDao.insertPayment(newpayment);
+        newpayment.setId(id);
 
         return newpayment.generateJwt();
     }
 
     @GetMapping("/payments/confirm")
-    public synchronized void confirmPayment(@RequestHeader("paymentId") UUID paymentId) throws IllegalArgumentException{
+    public synchronized void confirmPayment(@RequestHeader("paymentId") Long paymentId) throws IllegalArgumentException{
         /* Confirms payment with id paymentId and
         * inserts payment to database
         *
@@ -43,17 +65,21 @@ public class PaymentController {
         }
 
         Payment paymentToConfirm;
-        // TODO: Find payment in database
-        //       If payment doesn't exist: throw error
-
-        if (!paymentToConfirm.confirm()){
-            throw new IllegalArgumentException("Payment with ID " + paymentId + " doesn't exist");
+        try {
+            paymentToConfirm = paymentDao.selectPaymentById(paymentId);
+        } catch (EmptyResultDataAccessException e){
+            throw new IllegalArgumentException("Payment with ID" + paymentId + " doesn't exist");
         }
 
+        if (!paymentToConfirm.confirm()){
+            throw new IllegalArgumentException("Payment with ID " + paymentId + " is already confirmed");
+        }
+
+        paymentDao.updatePayment(paymentToConfirm);
     }
 
     @GetMapping("/payments/isconfirmed")
-    public Boolean isconfirmPayment(@RequestHeader("paymentId") UUID paymentId) throws IllegalArgumentException{
+    public Boolean isconfirmPayment(@RequestHeader("paymentId") Long paymentId) throws IllegalArgumentException{
         /* Throws exception if payment ID wasn't
         * specified or doesn't exist */
 
@@ -62,8 +88,11 @@ public class PaymentController {
         }
 
         Payment paymentToVerify;
-        // TODO: Find payment in database
-        //       If payment doesn't exist: throw error
+        try {
+            paymentToVerify = paymentDao.selectPaymentById(paymentId);
+        }catch (EmptyResultDataAccessException e){
+            throw new IllegalArgumentException("Payment with ID" + paymentId + " doesn't exist");
+        }
 
         return paymentToVerify.isConfirmed();
     }
