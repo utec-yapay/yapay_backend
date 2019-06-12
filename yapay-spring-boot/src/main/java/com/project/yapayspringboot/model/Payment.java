56264bc099203b6c7620bd4e2079a6bf254ede05
Arturo Cuya <arturocuya099@gmail.com>
2019-06-12 14:04:02 -0500

    Changed jwt library

--- yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/Payment.java
+++ yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/Payment.java
@@ -1,19 +1,14 @@
 package com.project.yapayspringboot.model;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.io.UnsupportedEncodingException;
-import java.security.Key;
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.UUID;
 
 public class Payment {
     private Long id;
@@ -33,21 +28,37 @@ public class Payment {
     public String generateJwt(){
         final Instant now = Instant.now();
 
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
         // TODO: Put this in a file
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("KbPeShVmYq3t6w9z$C&F)H@McQfTjWnZ-KbPeShVmYq3t6w9z$C&F)H@McQfTjWnZ-KbPeShVmYq3t6w9z$C&F)H@McQfTjWnZ-KbPeShVmYq3t6w9z$C&F)H@McQfTjWnZ");
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        String secret = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+        String token = "";
 
-        return Jwts.builder()
-                        .setIssuedAt(Date.from(now))
-                        .setExpiration(Date.from(now.plus(15, ChronoUnit.SECONDS)))
-                        .claim("pid", 123)
-                        .claim("amt", this.totalAmount)
-                        .claim("cpn", this.company.getName())
-                        .claim("cpp", this.company.getPhone())
-                        .signWith(signatureAlgorithm, signingKey)
-        .compact();
+        try {
+            Algorithm algorithmHS = Algorithm.HMAC256(secret);
+            token = JWT.create()
+                .withClaim("pid", 123)
+                .withClaim("cpn", "Drimer")
+                .withClaim("cpp", "993321323")
+                .sign(algorithmHS);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Algorithm algorithm = null;
+            try {
+                algorithm = Algorithm.HMAC256(secret);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .build(); //Reusable verifier instance
+            DecodedJWT jwt = verifier.verify(token);
+            System.out.println("VALID JWT");
+        } catch (JWTVerificationException exception){
+            System.out.println("INVALID JWT");
+        }
+
+        return token;
     }
 
     public boolean confirm(){
