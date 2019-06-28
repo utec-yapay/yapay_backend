de37bb1ad748fadf3674a982d595856f652e31fb
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-28 04:10:33 -0500

    Add Data Report and scheduler

---
+++ yapay-spring-boot/src/main/java/com/project/yapayspringboot/model/DataReportScheduler.java
@@ -0,0 +1,64 @@
+package com.project.yapayspringboot.model;
+
+import com.google.gson.Gson;
+import com.project.yapayspringboot.controller.PaymentController;
+import com.project.yapayspringboot.service.PaymentService;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+public class DataReportScheduler {
+    private static final Logger logger = Logger.getLogger(PaymentController.class.getName());
+
+    @Resource
+    PaymentService paymentService;
+
+    private String handlerUrl = "http://127.0.0.1:5001/handler/payments";
+
+
+    // @Scheduled(fixedRate = 10000) // For debugging only
+    
+    // cron expressions: second, minute, hour, day, month, weekday
+    @Scheduled(cron = "0 0 0 * * *") // midnight everyday
+    public void sendDataReport(){
+        DataReport dataReport = new DataReport();
+        dataReport.setPayments(paymentService.getYesterdayConfirmedPayments());
+
+        // Get dataReport serialized as JSON
+        Gson gson = new Gson();
+        StringEntity dataReportJson;
+        try {
+            dataReportJson = new StringEntity(gson.toJson(dataReport));
+        } catch (UnsupportedEncodingException e){
+            logger.log(Level.SEVERE, e.toString(), e);
+            return;
+        }
+
+        // Send POST to Yape handler
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(this.handlerUrl);
+        post.setEntity(dataReportJson);
+
+        try {
+            if (httpClient.execute(post).getStatusLine().getStatusCode()!=200){
+                logger.log(Level.FINE, "Data report sent successfully");
+            }
+        }catch (IOException e){
+            logger.log(Level.SEVERE, e.toString(), e);
+        }
+
+
+        System.out.println(dataReportJson);
+    }
+}
